e2273355508519f3105deb48af5e0551ea9b1aef
Paradiesworld <46368017+paradiesworld@users.noreply.github.com>
2019-01-10 23:19:11 -0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,21 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static for Jigsaw</h1>
+    <h1>Herzlich willkommen Ariana Grande Blog</h1>
 
-    <h2>Asset Compilation Test</h2>
+    <h2>Ariana Grande ist ein Star</h2>
 
-    <p>CSS test: <span class="test-css">text with border and padding</span></p>
+    <p>Ariana Grande Zu ihren Millionen Fans ist Grande ein Leitbild unverhohlener Weiblichkeit — und Feminismus.</p>
+    <p>Ariana Grande-Butera ist eine US-amerikanische Sängerin, Songwriterin und Schauspielerin. Geboren in Florida als Sohn einer italienischen Familie, begann sie 2008 ihre Karriere im Broadway-Musical 13, ehe sie die Rolle der Cat Valentine in der Nickelodeon Fernsehserie Victorious und im Spinoff Sam & Cat spielte.</p>
 
-    <p>JavaScript test: <a href="#" class="test-js">click me</a></p>
+    <p>Unser anderes Blog-Netzwerk ist : <a href="https://munchen.gq/">München.gq</a> ,<a href="https://munchen.cf">München.CF</a> ,<a href="https://munchen.ga">München.GA</a> ,
+<a href="https://munchen.ml">München.ML</a> , <a href="https://gesundheit.cf">Gesundheit.CF</a> ,<a href="https://pflege.gq">Pflege.GQ</a> ,
+<a href="https://baumfallung.tk">Baumfallung.TK</a> ,<a href="https://baumfallung.ml">Baumfallung.ML</a> ,<a href="https://bild.ga">Bild.GA</a></p>
+    <p>Unsere Kunden-Websites :  <a href="https://ihrbaumprofi.de">Ihrbaumprofi</a> | <a href="https://kieferorthopaede-fuenfhoefe.de">Kieferorthopäde München</a> | <a href="https://www.stefanrieth.com">Stefanrieth</a> |
+<a href="https://mittelzumleben.bz/nahrungsergaenzungsmittel/911/cbd-oel-5-vegan-10ml">CBD Öl</a> | <a href="https://delitip.com">Delitip</a> |
+</p>
+    <p>Für jeden Kontakt <form>
+<input style="width: 300px; padding: 20px; box-shaddow: 6px 6px 5px; #999999; -webkit-box-shadow: 6px 6px 5px #999999; -moz-box-shadow: 6px 6px 5px #999999; font-weight: bold; background: #CCFF00; color: #000000; cursor: pointer; border-radius: 10px; border: 1px solid #D9D9D9; font-size: 150%;" type="button" value="Send eMail" onclick="window.location.href='mailto:paradiesworld@gmail.com'" />
+</form></p>
+    <p></p>
 @endsection
