29f815c93c98cdbe8cc1b23b9a98ee6446634999
Paradiesworld <46368017+paradiesworld@users.noreply.github.com>
2019-01-10 23:48:25 -0800

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,16 +1,23 @@
 @extends('_layouts.master')
 
-@section('title', 'About')
+@section('title', 'About Ariana')
 
 @section('content')
-    <h1>About</h1>
+    <h1>Mehr über Ariana Grande</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
+    <p>Ariana wurde am 26. Juni 1993 in Boca Raton, Florida, als Sohn von Joan Marguerite Grande, einem Chief Executive Officer von Hose-McCann Communications & Edward Charles Butera, einem grafischen Designer-Gründer von IBI Designs Inc., geboren. Ihre Karriere begann sie 2008 im Broadway-Musical, 13, bevor sie die Rolle der Cat Valentine in der Nickelodeon TV-Serie, Victorious & in the spinoff, Sam & Cat bis 2014 spielte. Sie trat auch in anderen Theater-und Fernsehrollen auf und lieh ihre Stimme dem Animationsfernsehen und Filmen.
 
-    <h2>Links:</h2>
+Arianas Musikkarriere begann 2011 mit dem Soundtrack Music from Victorious. 2013 veröffentlichte sie ihr erstes Studioalbum Yours Truly, das auf dem US-Billboard 200 einstieg. Die Lead-Single des Albums, The Way, wurde in den Top 10 der Billboard Hot 100 eröffnet, und Kritiker vergleichen ihre breite Gesangsumfang mit der von Mariah Carey.
+
+Arianas zweites Studioalbum, My Everything (2014), wurde in den USA auf Platz eins aufgenommen und in 24 weiteren Ländern unter die Top 10 gekippt. Mit der Lead-Single, Problem und mehreren weiteren Singles war sie 34 Wochen lang kontinuierlich in den Top 10 der Billboard Hot 100 und hatte 2014 die Top 10 der Künstler. Im nächsten Jahr gab sie ihre erste Welttournee, The Honeymoon Tour, um My Everything zu promoten. 2016 veröffentlichte sie ihr drittes Studioalbum Dangerous Woman, das auf Platz zwei auf der Billboard 200 gechartert wurde. Der Titeltrack debütierte auf Platz 10 der Billboard Hot 100 und ist damit die erste Person in der Geschichte dieser Charts, die die Lead-Singles von jedem ihrer ersten drei Alben in den Top 10 in den USA hatte. 2017 gab Ariana ihre internationale Dangerous Woman Tour.
+
+Ab Juni 2017 waren Arianas Musikvideos insgesamt mehr als neun Milliarden Mal online angesehen worden. Zu ihren Auszeichnungen zählen drei American Music Awards, drei MTV Europe Music Awards, ein MTV Video Music Award und vier Grammy Award. Alle drei Alben wurden von der RIAA Platin zertifiziert. Sie hat eine Reihe von Wohltätigkeitsorganisationen unterstützt und hat eine große Anhängerschaft in sozialen Medien. 2016 nannte Time Ariana eine der 100 einflussreichsten Menschen der Welt auf ihrer Jahresliste.</p>
+
+    <h2>Quelle für Inhalte</h2>
 
     <ul>
-        <li><a href="https://twitter.com/{{ $page->owner->twitter }}" target="_blank">Twitter</a></li>
-        <li><a href="https://github.com/{{ $page->owner->github }}" target="_blank">GitHub</a></li>
+        <li><a href="https://www.instagram.com/arianagrande/" target="_blank">Instagram</a></li>
+        <li><a href="https://twitter.com/ArianaGrande" target="_blank">Twitter</a></li>
+        <li><a href="https://www.imdb.com/name/nm3812858/bio" target="_blank">imdb</a></li>
     </ul>
 @endsection
